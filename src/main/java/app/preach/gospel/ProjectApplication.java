b974efe02f739d8dd2dddb84655b203aed2911ff
ArkamaHozota <arkamahozota@hotmail.com>
2024-11-22 16:14:24 +0900

    1.18

--- src/main/java/app/preach/gospel/ProjectApplication.java
+++ src/main/java/app/preach/gospel/ProjectApplication.java
@@ -2,7 +2,7 @@ package app.preach.gospel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import app.preach.gospel.common.ProjectConstants;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +15,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @SpringBootApplication
-@ComponentScan(basePackages = { "app.preach.gospel.repository", "app.preach.gospel.service",
-		"app.preach.gospel.service.impl", "app.preach.gospel.handler" })
+@ServletComponentScan
 public class ProjectApplication {
 	public static void main(final String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
