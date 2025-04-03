07de86afafd3ed2a0344ab122275bd75553addc9
ArkamaHozota <arkamahozota@hotmail.com>
2025-04-03 21:25:41 +0900

    6.11

---
+++ src/main/java/app/preach/gospel/config/SpringWebMvcConfiguration.java
@@ -0,0 +1,26 @@
+package app.preach.gospel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import app.preach.gospel.common.ProjectConstants;
+import app.preach.gospel.common.ProjectURLConstants;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Configuration
+public class SpringWebMvcConfiguration implements WebMvcConfigurer {
+
+	/**
+	 * 静的なリソースのマッピングを設定する
+	 *
+	 * @param registry レジストリ
+	 */
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		log.info(ProjectConstants.MESSAGE_SPRING_MAPPER);
+		registry.addResourceHandler(ProjectURLConstants.URL_STATIC_RESOURCE).addResourceLocations("classpath:/static/");
+	}
+
+}
