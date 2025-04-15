e8cbf0cb6c0fd2c172a5ff4845ba4d9ec7e89726
ArkamaHozota <arkamahozota@hotmail.com>
2025-04-15 14:27:00 +0900

    6.73

--- src/main/java/app/preach/gospel/listener/ProjectExceptionHandler.java
+++ src/main/java/app/preach/gospel/listener/ProjectExceptionHandler.java
@@ -2,6 +2,7 @@ package app.preach.gospel.listener;
 
 import org.apache.struts2.dispatcher.DefaultDispatcherErrorHandler;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 
@@ -10,7 +11,6 @@ import com.alibaba.fastjson2.JSON;
 import app.preach.gospel.common.ProjectConstants;
 import app.preach.gospel.common.ProjectURLConstants;
 import app.preach.gospel.utils.CoProjectUtils;
-import jakarta.persistence.PersistenceException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
@@ -30,23 +30,26 @@ public final class ProjectExceptionHandler extends DefaultDispatcherErrorHandler
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		response.setCharacterEncoding(CoProjectUtils.CHARSET_UTF8.name());
 		try {
+			// エラーメッセージ
+			final String errorMessage = exception.getMessage();
 			// WW-1977: Only put errors in the request when code is a 500 error
 			if (exception instanceof AccessDeniedException) {
 				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 				response.getWriter().print(JSON.toJSON(ProjectConstants.MESSAGE_SPRINGSECURITY_REQUIRED_AUTH));
 				response.getWriter().close();
-			} else if (exception instanceof PersistenceException) {
+			} else if (exception instanceof DataAccessException) {
 				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				response.getWriter().print(JSON.toJSON(exception.getMessage()));
+				response.getWriter().print(JSON.toJSON(errorMessage));
 				response.getWriter().close();
 			} else {
 				// WW-4103: Only logs error when application error occurred, not Struts error
 				response.sendRedirect(ProjectURLConstants.URL_CATEGORY_NAMESPACE.concat(CoProjectUtils.SLASH)
-						.concat(ProjectURLConstants.URL_TO_ERROR).concat("?exception=").concat(exception.getMessage()));
+						.concat(ProjectURLConstants.URL_TO_ERROR).concat("?exception=").concat(errorMessage));
 			}
-			log.error("Exception occurred during processing request: {}", exception.getMessage());
+			log.error("Exception occurred during processing request: {}", errorMessage);
 		} catch (final Exception e) {
-			// Log illegalstate instead of passing unrecoverable exception to calling thread
+			// Log illegal state instead of passing unrecoverable exception to calling
+			// thread
 			log.warn("Unable to send error response, code: {}; isCommited: {};", code, response.isCommitted(), e);
 		}
 	}
